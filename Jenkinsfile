6b431457921053acbb782ea63c2c962479d4249a
nikiz995 <nikiz995@users.noreply.github.com>
2026-05-07 13:48:02 +0200

    Add Jenkins pipeline espresso tests

---
+++ Jenkinsfile
@@ -0,0 +1,127 @@
+pipeline {
+    agent any
+
+    environment {
+        AVD_NAME = "Pixel_6_API_35"
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/nikiz995/android-espresso-test.git'
+            }
+        }
+
+        stage('Force Clean') {
+            steps {
+                bat 'bundle exec fastlane android force_clean'
+            }
+        }
+
+        stage('Build APKs') {
+            steps {
+                bat 'bundle exec fastlane android build_tests'
+            }
+        }
+
+        stage('Start Emulator') {
+            steps {
+                bat '''
+                start /B emulator -avd %AVD_NAME% -no-snapshot -no-audio -no-boot-anim
+
+                adb wait-for-device
+
+                :checkboot
+                for /f "tokens=*" %%i in ('adb shell getprop sys.boot_completed') do set boot=%%i
+                if not "%boot%"=="1" (
+                    timeout /t 5
+                    goto checkboot
+                )
+
+                adb shell input keyevent 82
+                '''
+            }
+        }
+
+        stage('Run Espresso Tests and Screenshots') {
+            steps {
+                bat 'bundle exec fastlane android screenshots'
+            }
+        }
+    }
+
+    post {
+        always {
+            bat 'adb emu kill || exit /b 0'
+
+            archiveArtifacts artifacts: 'app/build/reports/androidTests/connected/**', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'app/build/outputs/androidTest-results/**', allowEmptyArchive: true
+
+            junit allowEmptyResults: true, testResults: 'app/build/outputs/androidTest-results/connected/**/*.xml'
+        }
+    }
+}pipeline {
+    agent any
+
+    environment {
+        AVD_NAME = "Pixel_6_API_35"
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/nikiz995/android-espresso-test.git'
+            }
+        }
+
+        stage('Force Clean') {
+            steps {
+                bat 'bundle exec fastlane android force_clean'
+            }
+        }
+
+        stage('Build APKs') {
+            steps {
+                bat 'bundle exec fastlane android build_tests'
+            }
+        }
+
+        stage('Start Emulator') {
+            steps {
+                bat '''
+                start /B emulator -avd %AVD_NAME% -no-snapshot -no-audio -no-boot-anim
+
+                adb wait-for-device
+
+                :checkboot
+                for /f "tokens=*" %%i in ('adb shell getprop sys.boot_completed') do set boot=%%i
+                if not "%boot%"=="1" (
+                    timeout /t 5
+                    goto checkboot
+                )
+
+                adb shell input keyevent 82
+                '''
+            }
+        }
+
+        stage('Run Espresso Tests and Screenshots') {
+            steps {
+                bat 'bundle exec fastlane android screenshots'
+            }
+        }
+    }
+
+    post {
+        always {
+            bat 'adb emu kill || exit /b 0'
+
+            archiveArtifacts artifacts: 'app/build/reports/androidTests/connected/**', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'app/build/outputs/androidTest-results/**', allowEmptyArchive: true
+
+            junit allowEmptyResults: true, testResults: 'app/build/outputs/androidTest-results/connected/**/*.xml'
+        }
+    }
+}
--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/screens/MainScreen.kt
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/screens/MainScreen.kt
@@ -2,81 +2,109 @@ package com.abnamro.apps.referenceandroid.screens
 
 import android.view.KeyEvent
 import androidx.annotation.StringRes
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.pressKey
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.platform.app.InstrumentationRegistry
 import com.abnamro.apps.referenceandroid.R
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 
 class MainScreen {
 
-    fun verifyToolbarTitleIsDisplayed(): MainScreen {
-        verifyTextIsDisplayed(R.string.app_name)
-        return this
+    // =========================
+    // Locators
+    // =========================
+
+    private val rootView = isRoot()
+
+    private val floatingActionButton =
+        allOf(withId(R.id.fab), isDisplayed())
+
+    private val settingsMenuOption =
+        withText(R.string.action_settings)
+
+    private val snackbarMessage =
+        allOf(
+            withId(com.google.android.material.R.id.snackbar_text),
+            withText("Replace with your own action")
+        )
+
+    // =========================
+    // Actions
+    // =========================
+
+    fun openSettingsMenu() = apply {
+    openActionBarOverflowOrOptionsMenu(
+        InstrumentationRegistry.getInstrumentation().targetContext
+    )
+}
+
+    fun selectSettings() = apply {
+        onView(settingsMenuOption)
+            .perform(click())
     }
 
-    fun verifyWelcomeMessageIsDisplayed(): MainScreen {
-        verifyTextIsDisplayed("Hello World!")
-        return this
+    fun tapFloatingActionButton() = apply {
+        onView(floatingActionButton)
+            .perform(click())
     }
 
-    fun openSettingsMenu(): MainScreen {
-        onView(isRoot()).perform(pressKey(KeyEvent.KEYCODE_MENU))
-        return this
-    }
-
-    fun selectSettings(): MainScreen {
-        onView(withText(R.string.action_settings)).perform(click())
-        return this
-    }
-
-    fun tapFloatingActionButton(): MainScreen {
-        onView(allOf(withId(R.id.fab), isDisplayed())).perform(click())
-        return this
-    }
-
-    fun tapFloatingActionButton(times: Int): MainScreen {
+    fun tapFloatingActionButton(times: Int) = apply {
         repeat(times) {
             tapFloatingActionButton()
             verifySnackbarMessageIsDisplayed()
         }
-        return this
     }
 
-    fun pressDeviceBack(): MainScreen {
+    fun pressDeviceBack() = apply {
         pressBack()
-        return this
     }
 
-    fun performRepeatedMixedInteractions(): MainScreen {
+    fun performRepeatedMixedInteractions() = apply {
         tapFloatingActionButton()
         openSettingsMenu()
         pressDeviceBack()
         tapFloatingActionButton()
         openSettingsMenu()
         selectSettings()
-        return this
     }
 
-    fun verifySnackbarMessageIsDisplayed(): MainScreen {
-        onView(allOf(
-            withId(com.google.android.material.R.id.snackbar_text),
-            withText("Replace with your own action")
-        )).check(matches(isDisplayed()))
-        return this
+    // =========================
+    // Verifications
+    // =========================
+
+    fun verifyToolbarTitleIsDisplayed() = apply {
+        verifyTextIsDisplayed(R.string.app_name)
     }
+
+    fun verifyWelcomeMessageIsDisplayed() = apply {
+        verifyTextIsDisplayed("Hello World!")
+    }
+
+    fun verifySnackbarMessageIsDisplayed() = apply {
+        onView(snackbarMessage)
+            .check(matches(isDisplayed()))
+    }
+
+    // =========================
+    // Reusable Methods
+    // =========================
 
     private fun verifyTextIsDisplayed(@StringRes textResId: Int) {
-        onView(withText(textResId)).check(matches(isDisplayed()))
+        onView(withText(textResId))
+            .check(matches(isDisplayed()))
     }
 
     private fun verifyTextIsDisplayed(text: String) {
-        onView(withText(text)).check(matches(isDisplayed()))
+        onView(withText(text))
+            .check(matches(isDisplayed()))
     }
 }
